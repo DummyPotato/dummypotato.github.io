6723c715a982d9ad8ff46d9519c2d5bc56fe893b
DummyPotato <94937449+DummyPotato@users.noreply.github.com>
2024-05-26 10:27:36 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,10 +2,15 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
     const { url } = req.query;
+    if (!url) {
+        return res.status(400).send('Bad Request: Missing URL parameter');
+    }
+
     try {
         const response = await axios.get(url);
         res.send(response.data);
     } catch (error) {
+        console.error('Error fetching URL:', error.message);
         res.status(500).send('Error fetching the URL');
     }
 };
