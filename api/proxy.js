375d5b03b3d07b6b195c5c05312ffac161c7fa18
DummyPotato <94937449+DummyPotato@users.noreply.github.com>
2024-05-26 11:08:47 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -15,7 +15,7 @@ app.get('/proxy', async (req, res) => {
     }
 
     try {
-        console.log(`Fetching URL: ${url}`);
+        console.log(`Fetching URL: ${CORS_PROXY + url}`);
         const response = await axios.get(CORS_PROXY + url, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
