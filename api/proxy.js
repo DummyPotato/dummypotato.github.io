ce07260820da50076ec4a03ae67fbe6290cf38e4
DummyPotato <94937449+DummyPotato@users.noreply.github.com>
2024-05-26 10:46:00 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 app.get('/proxy', async (req, res) => {
     const { url } = req.query;
